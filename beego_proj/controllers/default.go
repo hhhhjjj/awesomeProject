a5c6b4ebee26f0ab94dac234a8e1fe0adf01082b
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2020-03-27 21:17:27 +0800

    下一步连接mysql

--- beego_proj/controllers/default.go
+++ beego_proj/controllers/default.go
@@ -9,7 +9,10 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	//这里一个get请求
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
+	//这个data表示返回数据字段和内容
 	c.TplName = "index.tpl"
+//	表示指向的模板文件，在view里面
 }
--- beego_proj/main.go
+++ beego_proj/main.go
@@ -2,8 +2,10 @@ package main
 //在goland里面不需要用那些命令，直接配置好之后右上角点运行就可以了
 //至于使用bee run导致的报错懒得去研究了
 //其实这个和django差不多，也是那种MVC的
+//go语言的执行流程：先routers里面执行init再到main的run里面
 import (
 	_ "awesomeProject/beego_proj/routers"
+	//这个下划线表示是引入routers包并且执行init方法
 	"github.com/astaxie/beego"
 )
 
